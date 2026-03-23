3b5f0f82b25d3b5b16be20ad928dfdf230c1766a
kimjhkim11 <kimjhkim11@gmail.com>
2026-03-23 16:00:03 +0900

    Update page.streamlit_app.py

--- page.streamlit_app.py
+++ page.streamlit_app.py
@@ -184,7 +184,7 @@ def generate_jeet_expert_report(target_name, selected_test):
                 ax2.set_ylim(0, max_val * 1.4) # 숫자 라벨을 위한 위쪽 여백
                 
                 ax2.set_title("▶ 단원별 성취도", pad=30, fontsize=14, fontweight='bold', color=COLOR_NAVY)
-                ax2.legend(loc='upper center', bbox_to_anchor=(0.5, 1.25), ncol=2, fontsize=8, frameon=False)
+                ax2.legend(loc='upper center', bbox_to_anchor=(0.5, 1.12), ncol=2, fontsize=8, frameon=False)
                 ax2.grid(axis='y', color=COLOR_GRID, linestyle='--', linewidth=0.5, zorder=0)
                 
                 # 4. 막대 위에 점수 텍스트 표시
